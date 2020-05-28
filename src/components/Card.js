a62e703f4aa1b87a1ba349862ca85cf38596037b
Rob W <robpwatkins@gmail.com>
2020-05-28 11:37:13 -0500

    got some color on there yalllll

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Card = (props) => {
+
+  let cardClass = [
+    'card',
+    ((props.card.suit === 'Diamonds') || (props.card.suit === 'Hearts')) && 'red'
+  ];
+  cardClass = cardClass.join(' ');
+
   return (
-    <div className="card">
+    <div className={cardClass}>
       {props.card.rank} of {props.card.suit}
     </div>
   )
